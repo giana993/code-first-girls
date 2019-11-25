015e7d1fde39b61657c4a161b3f1f8b085aa4687
giana993 <37670040+giana993@users.noreply.github.com>
2019-11-25 22:08:53 +0000

    Added various features

--- something.js
+++ something.js
@@ -1,18 +1,20 @@
-var aText = new Array(
+var aText2 = new Array(
 "Skhi.",
 "Welcome to my world."
 );
+var aText3 = new Array("Lyrics about sky - artist.");
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
+ // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
 
 var iTextPos = 0; // initialise text position
 var sContents = ''; // initialise contents variable
 var iRow; // initialise current row
 
-function typewriter()
+function typewriter(aText, index)
 {
+    var iArrLength = aText[index].length;
  sContents =  ' ';
  iRow = Math.max(0, iIndex-iScrollAt);
  var destination = document.getElementById("typedtext");
@@ -26,12 +28,10 @@ function typewriter()
   iIndex++;
   if ( iIndex != aText.length ) {
    iArrLength = aText[iIndex].length;
-   setTimeout("typewriter()", 500);
+   setTimeout(function(){ typewriter(aText, iIndex)}, 500);
   }
  } else {
-  setTimeout("typewriter()", iSpeed);
+  setTimeout(function(){typewriter(aText, index)}, iSpeed);
  }
 }
 
-
-typewriter();
